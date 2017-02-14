04d7b19518e19782d74dba877e22a3b7bf5abf14
sudha singh <sudha.suryavanshi@gmail.com>
2017-02-14 15:46:48 +0530

    committed successfully

---
+++ online-shoppingbackend/src/main/java/net/sudha/online_shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sudha.online_shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
